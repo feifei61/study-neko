02751a4f5013beae94353c914384692606a7dbf6
Faye Chen <faye2011@kimo.com>
2023-06-15 18:47:07 +1000

    nextjs

---
+++ src/components/Status.tsx
@@ -0,0 +1,17 @@
+type Props = {
+    earnings: number;
+}
+
+const Status = ({ earnings } : Props) => {
+
+    return (
+        <div className='w-1/4 text-white font-outfit'>
+            <div className="w-10 bg-amber-400 text-center text-amber-700 font-semibold rounded border-2 border-amber-500">
+                <span>${' '}</span>
+                {earnings}
+            </div>
+        </div>
+    )
+}
+
+export default Status
